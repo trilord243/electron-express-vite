2914f2fc175542a34f9ec0c98dfbb26ca15fca96
Felipe Escalona <escalonaf12@gmail.com>
2024-08-19 20:18:57 -0400

    nodejs and electron app

---
+++ electron/main.ts
@@ -0,0 +1,72 @@
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "node:url";
+import express, { Request, Response } from "express";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+process.env.APP_ROOT = path.join(__dirname, "..");
+
+export const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
+export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
+
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
+  ? path.join(process.env.APP_ROOT, "public")
+  : RENDERER_DIST;
+
+let win: BrowserWindow | null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.mjs"),
+    },
+  });
+
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
+}
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
+  }
+});
+
+app.on("ready", () => {
+  createWindow();
+
+  // Inicia el servidor Express
+  const expressApp = express();
+
+  expressApp.get("/api/mensaje", (req: Request, res: Response) => {
+    console.log(req);
+    res.send({ message: "Marico el que lo lea" });
+  });
+
+  expressApp.listen(3001, () => {
+    console.log("Servidor Express corriendo en el puerto 3001");
+  });
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.whenReady().then(() => {
+  if (!win) {
+    createWindow();
+  }
+});
